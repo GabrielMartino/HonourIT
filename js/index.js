6d81b83d483bd300c328ccf2656c0a7ac8e83ab9
Gabriel Martino <GabrielMartino@users.noreply.github.com>
2022-10-29 19:00:52 +1000

    made amazing progress woohoo

--- js/index.js
+++ js/index.js
@@ -4,14 +4,40 @@ $(document).ready(function(){
       
 
 
-      if ($(".honour").attr("src")) {
-        $(".honour").attr("src", "images/icon-honour-selected.svg");
+      if ($(".honour").attr("src") == "images/icon-honour.svg") {
+ 
+        $(this).attr("src", "images/icon-honour-selected.svg");
+        $(".honourvalue").html("27");
+
       } 
-         else {
-          $(".honour").attr("src", "images/icon-honour.svg");
+         else  {
+
+ 
+          $(this).attr("src", "images/icon-honour.svg");
+          $(".honourvalue").html("26");
       }
     });
+  })
 
+
+  $(document).ready(function(){
+
+var chadimage = '<button class="button-toimage chad-image">'+
+'<img src="images/icon-bin.svg" class="remove-image">'+
+'</button>'
+
+    $("#add-image").click(function(){
+      $('#images').append(chadimage)
+
+    });
+  })
+
+  $(document).ready(function(){
+
+  
     
-
-  });
+        $(".remove-image").click(function(){
+          $(this).closest('.chad-image').remove();
+    console.log('work dammit');
+        });
+      })
